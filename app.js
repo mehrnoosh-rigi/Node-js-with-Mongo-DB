628d7057426537037292a463586b428159008293
Mehrnoosh Rigi <>
2022-11-11 13:38:11 +0100

    views handlebar

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const morgan = require("morgan")
+const exphbs = require("express-handlebars")
+// const  engine = require('express-handlebars')
+
 const connectDB = require("./config/db")
 
 //load config
@@ -8,6 +12,17 @@ dotenv.config({path: "./config/config.env"})
 connectDB()
 
 const app = express()
+
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan("dev")) // shows response of http method on console
+}
+
+//handlebars
+// app.engine('.hbs', engine());
+// app.set('view engine', '.hbs');
+// app.set('views', './views');
+app.engine(".hbs", exphbs.engine({defaultLayout: "main" , extname: ".hbs"}))
+app.set("view engine", ".hbs")
 
 const PORT = process.env.PORT || 5000
 
