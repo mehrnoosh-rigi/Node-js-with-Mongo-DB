d371e28e399639341af86b932829c8cbd7d87184
Mehrnoosh Rigi <>
2022-11-15 13:31:36 +0100

    Use passport and add user module for authentication with Google

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+const path = require("path")
 const express = require("express")
 const dotenv = require("dotenv")
 const morgan = require("morgan")
 const exphbs = require("express-handlebars")
+const passport = require("passport")
+const session = require("express-session")
 
 const connectDB = require("./config/db")
 
 //load config
 dotenv.config({path: "./config/config.env"})
+
+//passport config
+require("./config/passport")(passport)
 
 connectDB()
 
@@ -20,8 +26,23 @@ if(process.env.NODE_ENV === "development"){
 app.engine(".hbs", exphbs.engine({defaultLayout: "main" , extname: ".hbs"}))
 app.set("view engine", ".hbs")
 
+//sessions
+app.use(session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false
+}))
+
+//passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
+//static folder
+app.use(express.static(path.join(__dirname, "public")))
+
 //routes
 app.use("/", require("./routes/index"))
+app.use("/auth", require("./routes/auth"))
 
 const PORT = process.env.PORT || 5000
 
