eb0a7fd6206a76254804e7c8f82443ea48878fe1
Mehrnoosh Rigi <>
2022-11-17 12:33:40 +0100

    navbar

--- routes/auth.js
+++ routes/auth.js
@@ -12,5 +12,11 @@ router.get("/google/callback", passport.authenticate("google", {
     failureRedirect: "/"
 }), (req, res) => {res.redirect("/dashboard")})
 
+//@desc Logout user
+//@route GET /auth/logout
+router.get("/logout", (req, res) => {
+    req.logOut()
+    res.redirect("/")
+})
 
 module.exports = router
